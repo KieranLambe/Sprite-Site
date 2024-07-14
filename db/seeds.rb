bfe1a1578f6db6c42a7203ac795390ca6500fdce
Kieran Lambe <lambekieran@me.com>
2024-07-14 13:15:44 +0100

    items populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Item.destroy_all
+
+items_data = [
+  { name: 'Oldest Sprite', description: 'From a simpler time. Price not adjusted for inflation.', price: 0.10, star_rating: 4 },
+  { name: 'Big Sprite Bottle', description: 'For the large of thirst. You asked, Sprite answered.', price: 3.00, star_rating: 3 },
+  { name: 'Sprite Can Classic', description: 'Timeless can design, it does not get better than this.', price: 1.00, star_rating: 5 },
+  { name: 'Lightly-used Sprite Can', description: 'This one has seen better days. Actual Sprite sold separately.', price: 0.34, star_rating: 2 }
+]
+
+items_data.each do |item_data|
+  item = Item.create!(item_data)
+  item.image.attach(io: File.open(Rails.root.join('app/assets/images', "#{item_data[:name].parameterize}.png")), filename: "#{item_data[:name].parameterize}.png")
+end 
+
+puts "Seeded #{Item.count} items."

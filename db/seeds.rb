f5a5edf7cb2968f6ea3df5298dc97cc814dc91a9
Kieran Lambe <lambekieran@me.com>
2024-07-20 00:32:05 +0100

    seeded locally

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,16 @@
+BasketItem.destroy_all
 Item.destroy_all
 
 items_data = [
-  { name: 'Oldest Sprite', description: 'From a simpler time. Price not adjusted for inflation.', price: 0.10, star_rating: 4 },
-  { name: 'Big Sprite Bottle', description: 'For the large of thirst. You asked, Sprite answered.', price: 3.00, star_rating: 3 },
-  { name: 'Sprite Can Classic', description: 'Timeless can design, it does not get better than this.', price: 1.00, star_rating: 5 },
-  { name: 'Lightly-used Sprite Can', description: 'This one has seen better days. Actual Sprite sold separately.', price: 0.34, star_rating: 2 }
+  { name: 'Oldest Sprite', description: 'From a simpler time. Price not adjusted for inflation.', price: 0.10, star_rating: 4, image_filename: 'oldest-sprite.png' },
+  { name: 'Big Sprite Bottle', description: 'For the large of thirst. You asked, Sprite answered.', price: 3.00, star_rating: 3, image_filename: 'big-sprite-bottle.png' },
+  { name: 'Sprite Can Classic', description: 'Timeless can design, it does not get better than this.', price: 1.00, star_rating: 5, image_filename: 'sprite-can-classic.png' },
+  { name: 'Lightly-used Sprite Can', description: 'This one has seen better days. Actual Sprite sold separately.', price: 0.34, star_rating: 2, image_filename: 'lightly-used-sprite-can.png' }
 ]
 
 items_data.each do |item_data|
   item = Item.create!(item_data)
-  item.image.attach(io: File.open(Rails.root.join('app/assets/images', "#{item_data[:name].parameterize}.png")), filename: "#{item_data[:name].parameterize}.png")
+  item.image.attach(io: File.open(Rails.root.join('app/assets/images/items', "#{item_data[:name].parameterize}.png")), filename: "#{item_data[:name].parameterize}.png")
 end 
 
 puts "Seeded #{Item.count} items."

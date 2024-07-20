f632c1364b695e7753669f120da41264311be284
Kieran Lambe <lambekieran@me.com>
2024-07-20 23:55:28 +0100

    fresh seed seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ items_data = [
   { name: 'Oldest Sprite', description: 'From a simpler time. Price not adjusted for inflation.', price: 0.10, star_rating: 4, image_filename: 'oldest-sprite.png' },
   { name: 'Small Sprite Can', description: 'Little micro-can. We cannot say how small exactly (very small).', price: 0.50, star_rating: 5, image_filename: 'small-sprite-can.png' },
   { name: 'Cranberry Sprite', description: 'Bet you did not know we did this. Well hah! We do.', price: 1.00, star_rating: 1, image_filename: 'cranberry-sprite.png' },
-  { name: 'Minimalist Sprite Can', description: 'For those not looking to cause a splash. Truly the old money of sprite cans.', price: 10.00, star_rating: 5, image_filename: 'minimalist-sprite.png' },
+  { name: 'Minimalist Sprite', description: 'For those not looking to cause a splash. Truly the old money of sprite cans.', price: 10.00, star_rating: 5, image_filename: 'minimalist-sprite.png' },
   { name: 'Coldest One', description: 'Ice cool, ideal for cracking in male company', price: 2.00, star_rating: 3, image_filename: 'coldest-one.png' },
   { name: 'Bad Decisions Sprite', description: 'Absolutely a great idea. Regret never tasted so fresh.', price: 4.00, star_rating: 2, image_filename: 'bad-decisions-sprite.png' }
 ]
